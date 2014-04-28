7f1feddc802441d97b1eca1fce49b2b77af04153
Javier Esquivel <javiereskivel@gmail.com>
2014-04-28 01:06:36 -0500

    Status

--- app.js
+++ app.js
@@ -38,12 +38,23 @@ app.post('/add', function(req, res){
 });
 
 
+var status = 0;
 //Servicio de prendido y apagado
 app.get('/on', function(req, res){
 	var query = require('url').parse(req.url,true).query;
 	var status = req.params;
 	console.log(query.status);
 	if(query.status == 1){
+		status =1;
+		res.send ("status=1");
+	}else{
+		status =0;
+		res.send ("status=0");
+	}
+});
+
+app.get('/status', function(req, res){
+	if(status == 1){
 		res.send ("status=1");
 	}else{
 		res.send ("status=0");
