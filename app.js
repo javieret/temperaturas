af90fc59626b277a76f3ffc07d052296e673a498
Javier Esquivel <javiereskivel@gmail.com>
2014-05-16 14:35:44 -0500

    Log temperaturas

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/temperatura', function(req, res){
 
 
 //Obtener Todas Las Temperaturas
-app.get('/getAll', function(req, res){
+app.post('/getAll', function(req, res){
 	return res.send(JSON.stringify(temperatura_array));
 });
 
